109c4d1b63a649fd1844460b8b5961782cfcacb4
Blake Woodworth <blakewoodworth@gmail.com>
2024-01-16 11:34:19 -0500

    initial commit

---
+++ reproduce.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os
+import random
+import torch
+import torchvision
+from tqdm import tqdm
+import torchvision.transforms as transforms
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid", context="talk", palette=sns.color_palette("bright"), color_codes=False)
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+np.set_printoptions(precision=3, linewidth=160)
+
+
+
+
+
+
+
